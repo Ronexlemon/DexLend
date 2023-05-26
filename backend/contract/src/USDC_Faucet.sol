c63004cf8d151b3dae3d754e652fcd147d07754b
RonexLemon <ronexondimu@gmail.com>
2023-05-26 19:55:56 +0300

    add usdc Faucet

---
+++ backend/contract/src/USDC_Faucet.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH IS NOT AUDITED
+ * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
+ */
+contract USDCFaucet is ERC20 {
+    uint time_toMintNext = 14400;
+    mapping(address => uint) public timeWaitToMint;
+
+    constructor() ERC20("USDC", "USDC") {}
+
+    function mintTokens(address user, uint amount) public {
+        require(amount > 1000 ether, "can only mint 1000");
+        require(timeWaitToMint[user] < block.timestamp, "need to wait");
+        timeWaitToMint[user] = block.timestamp + time_toMintNext;
+
+        _mint(user, amount);
+    }
+
+    function returnTime(address user) public view returns (uint) {
+        if (timeWaitToMint[user] > block.timestamp) {
+            return timeWaitToMint[user];
+        } else {
+            return 0;
+        }
+    }
+}

bacc3a1286271135fb42449368fadd41fb633fde
RonexLemon <ronexondimu@gmail.com>
2023-05-27 19:07:26 +0300

    final commit

--- client/src/components/homeCard.jsx
+++ client/src/components/homeCard.jsx
@@ -6,7 +6,7 @@ const HomeCard = () => {
 
   return (
     <div className="inset-0 flex justify-center mt-10 h-3/4">
-      <div className="w-3/4 p-6 border border-gray-400 rounded-lg shadow bg-bgbank dark:bg-gray-800 dark:border-gray-700" id="open-btn">
+      <div className="w-3/4 p-6 border border-gray-400 rounded-lg shadow bg-bgbank  bg-cover bg-no-repeat dark:bg-gray-800 dark:border-gray-700" id="open-btn">
        
 <div className="flex justify-center items-center h-3/4"><span className=" text-7xl animate-ping text-black">DEXLEND</span></div>
 <div className="flex justify-center items-center">
